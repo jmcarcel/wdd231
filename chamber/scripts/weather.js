85cddc50a85e0d478df354407fe3e1885acfa177
jmcarcel <carceljoan@gmail.com>
2025-08-02 20:33:24 -0300

    chamber home page

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,15 +1,83 @@
-    function calculateWindChill(temp, wind) {
-        if (temp > 10 || wind < 4.8) { return "N/A"; }
-        const windChill = 13.12 + 0.6215 * temp - 35.75 * Math.pow(wind, 0.16) + 0.4275 * temp * Math.pow(wind, 0.16);
-        return windChill.toFixed(1);
-}
-    const temperature = 10
-const windSpeed = 5
-    
-document.getElementById("temp").textContent = temperature.toFixed(1);
-document.getElementById("wind").textContent = windSpeed.toFixed(1);
-document.getElementById("windChill").textContent = calculateWindChill(temperature, windSpeed);
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
 
-temperature.innerHTML = `<span>${temperature.toFixed(1)}</span>`;
-windSpeed.innerHTML = `<span>${windSpeed.toFixed(1)}</span>`;
-windChill.innerHTML = `<span>${calculateWindChill(temperature, windSpeed)}</span>`;
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-32.95&lon=-60.69&appid=093aea2f8d23da3b4d5838f48f4e5459&units=metric';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            //console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+
+    currentTemp.innerHTML = `${(data.main.temp)}&deg;C`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+
+    let desc = data.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+
+    captionDesc.textContent = `${desc}`;
+}
+
+// Weather forecast
+
+const todayTemp = document.querySelector('#today-temp');
+const tomorrowTemp = document.querySelector('#tomorrow-temp');
+const afterTomorrowTemp = document.querySelector('#aftertomorrow-temp');
+
+function displayWeekdays() {
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const today = new Date();
+    const todayIndex = today.getDay();
+
+    const todayName = days[todayIndex];
+
+    const tomorrowIndex = (todayIndex + 1) % 7;
+    const tomorrowDayName = days[tomorrowIndex];
+
+    const dayAfterTomorrowIndex = (todayIndex + 2) % 7;
+    const dayAfterTomorrowName = days[dayAfterTomorrowIndex];
+
+    todayTemp.innerHTML = `${todayName}: `;
+    tomorrowTemp.innerHTML = `${tomorrowDayName}: `;
+    afterTomorrowTemp.innerHTML = `${dayAfterTomorrowName}: `;
+}
+
+displayWeekdays()
+
+async function apiFetchForecast() {
+    let urlToday = `https://api.openweathermap.org/data/2.5/weather?lat=-32.95&lon=-60.69&appid=093aea2f8d23da3b4d5838f48f4e5459&units=metric`;
+
+    //console.log(urlToday);
+    try {
+        const response = await fetch(urlToday);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            todayTemp.innerHTML += `${(data.list[4].main.temp)}&deg;C`;
+            tomorrowTemp.innerHTML += `${(data.list[12].main.temp)}&deg;C`;
+            afterTomorrowTemp.innerHTML += `${(data.list[20].main.temp)}&deg;C`;
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetchForecast();
