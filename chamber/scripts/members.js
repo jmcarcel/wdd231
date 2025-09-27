3123b497c473f9811ce3c24f7a7d614a00ef0fa5
jmcarcel <carceljoan@gmail.com>
2025-09-27 18:45:03 -0300

    assignment 02

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,11 +1,11 @@
-const companies = 'https://github.com/jmcarcel/wdd231/blob/main/chamber/data/members.json';
+const companies = 'https://jmcarcel.github.io/wdd231/chamber/data/members.json';
 
 const cards = document.querySelector('#companies');
 
 async function getMembers() {
     const response = await fetch(companies);
     const data = await response.json(); // parse the JSON data
-    // console.table(data.members);
+    console.table(data.members);
     displayMembers(data.members);
 }
 
